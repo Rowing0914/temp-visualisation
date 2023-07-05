8ec8056f48b42418ec437a663d48aa750855480e
Rowing0914 <kosakaboat@gmail.com>
2023-07-05 12:55:31 +0000

    update

--- b.py
+++ b.py
@@ -5,6 +5,10 @@ import numpy as np
 from numpy import exp, pi, linspace
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+
+if not os.path.exists("./images"):
+    os.makedirs("./images")
 
 sns_colors = sns.color_palette("Set1", 10)
 
@@ -12,14 +16,25 @@ def circle(radius, center):
     theta = linspace(0, 2 * pi, 200)
     return center + radius * exp(1j * theta)
 
-def plot_curves(curves, sing_pt=None, fname="./images/a.jpg"):
-    for i, c in enumerate(curves):
+def plot_curves(before_curves, after_curves, sing_pt=None, fname="./images/a.jpg"):
+    plt.subplot(121)
+    for i, c in enumerate(before_curves):
         plt.plot(c.real, c.imag, color=sns_colors[i], label=f"{i}")
         plt.scatter(c.real[0], c.imag[0], color=sns_colors[i], marker="^")
-        # print(c.real[0], c.imag[0])
     if sing_pt is not None:
         plt.scatter(sing_pt.real, sing_pt.imag, color=sns_colors[i + 1], marker="s", label=f"Sing")
-    plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=5, fancybox=True, shadow=True)
+    plt.title("Before transform")
+    plt.gca().set_aspect('equal')
+
+    plt.subplot(122)
+    for i, c in enumerate(after_curves):
+        plt.plot(c.real, c.imag, color=sns_colors[i], label=f"{i}")
+        plt.scatter(c.real[0], c.imag[0], color=sns_colors[i], marker="^")
+    # plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    # plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3, fancybox=True, shadow=True)
+    # plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=5, fancybox=True, shadow=True)
+    plt.title("After transform")
     plt.gca().set_aspect('equal')
     plt.savefig(fname)
     plt.clf()
@@ -29,33 +44,31 @@ def mobius(z, a, b, c, d):
 
 # # scale circles
 # circles = [circle(1, 0), circle(2, 0), circle(2, 6)]
-# plot_curves(circles, sing_pt=-4/3 + 0.0j, fname="./images/circles.jpg")
-# plot_curves([mobius(c, 2, 0, 0, 1) for c in circles], fname="./images/circles-scaled.jpg")
+# plot_curves(before_curves=circles, after_curves=[mobius(c, 2, 0, 0, 1) for c in circles], fname="./images/circles-scaled.jpg")
 
 # # rotate by a degree in radian
 # # Ref: Example 3.6 of http://www.warwickmaths.com/wp-content/uploads/2020/07/80_-M%C3%B6bius-Transformations.pdf
 # R = 1
-# for _theta in range(0, 361, 45):
+# for _theta in range(0, 361, 90):
 #     __theta = np.radians(_theta)
 #     a = R * np.exp(eval(f"{__theta}j"))
-#     plot_curves([mobius(c, a, 0, 0, 1) for c in circles], fname=f"./images/circles-rotate{_theta}.jpg")
+#     plot_curves(before_curves=circles, after_curves=[mobius(c, a, 0, 0, 1) for c in circles], fname=f"./images/circles-rotate{_theta}.jpg")
 
-# Inversion of circles
-plot_curves([mobius(c, 0, 1, 1, 0) for c in circles], fname="./images/circles-inv.jpg")
+# # Inversion of circles
+# plot_curves(before_curves=circles, after_curves=[mobius(c, 0, 1, 1, 0) for c in circles], sing_pt=0.0, fname="./images/circles-inv.jpg")
 
-# circle to circle
-plot_curves([mobius(c, 1, 2, 3, 4) for c in circles], fname="./images/circles-transformed.jpg")
+# # circle to circle
+# plot_curves(before_curves=circles, after_curves=[mobius(c, 1, 2, 3, 4) for c in circles], sing_pt=-4/3 + 0.0j, fname="./images/circles-transformed.jpg")
 
 # line to line
-line = linspace(-100, 100, 600)
-curves = [line, 1j * line - 4/3]
-plot_curves(curves, fname="./images/lines.jpg")
-plot_curves([mobius(c, 1, 2, 3, 4) for c in curves], fname="./images/lines-transformed.jpg")
+line = linspace(-20, 20, 600)
+curves = [1j * line - 4/3, 1j * line + 4/3, 1j * line - 2]
+plot_curves(before_curves=curves, after_curves=[mobius(c, 1, 2, 3, 4) for c in curves], sing_pt=-4/3 + 0.0j, fname="./images/line2line.jpg")
+plot_curves(before_curves=curves, after_curves=[mobius(c, 0, 1, 1, 0) for c in curves], sing_pt=0.0, fname="./images/line-inv.jpg")
 
 # line to circle
 lines = [1j * line - 4, 1j * line + 4, line - 4j, line + 4j]
-plot_curves(lines, fname="./images/line-to-circle.jpg")
-plot_curves([mobius(c, 1, 2, 3, 4) for c in lines], fname="./images/line-to-circle-transformed.jpg")
+plot_curves(before_curves=lines, after_curves=[mobius(c, 1, 2, 3, 4) for c in lines], sing_pt=-4/3 + 0.0j, fname="./images/line2circle.jpg")
 
 # Face
 dash = linspace(0.60, 0.90, 20)
@@ -63,7 +76,4 @@ smile = 0.3*exp(1j*2*pi*dash) - 0.2j
 left_eye  = circle(0.1, -0.4+.2j)
 right_eye = circle(0.1,  0.4+.2j)
 face = [circle(1, 0), left_eye, smile, right_eye]
-plot_curves(face, fname="./images/face.jpg")
-plot_curves([mobius(c, 1, 0, 1, -1 + 1j) for c in face], fname="./images/face-transformed-1.jpg")
-plot_curves([mobius(c, 1, 0, 3, 1) for c in face], fname="./images/face-transformed-2.jpg")
-plot_curves([mobius(c, 1, 0, 1, 0.4-0.2j) for c in face], fname="./images/face-transformed-3.jpg")
+plot_curves(before_curves=face, after_curves=[mobius(c, 1, 2, 3, 4) for c in face], sing_pt=-4/3 + 0.0j, fname="./images/face-transformed.jpg")
